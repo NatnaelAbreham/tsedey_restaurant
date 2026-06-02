2d662880804b4eeea5c427fd23f871ccd5faae18
NatnaelAbreham <natiabreham728@gmail.com>
2026-06-02 14:20:31 +0300

    exploring useeffect and usestate

--- tsedey_restaurant/src/App.jsx
+++ tsedey_restaurant/src/App.jsx
@@ -1,5 +1,4 @@
-// src/App.jsx
-import React, { useState, useEffect } from 'react'
+import  { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Menu from './components/Menu'
